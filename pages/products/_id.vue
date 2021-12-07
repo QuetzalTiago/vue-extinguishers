354f1c40e5f5f2b7a84e298aba27cddd2865862f
Tiago Romero <quetzaltiago@gmail.com>
2021-12-07 10:59:04 -0300

    Add(-): products page

---
+++ pages/products/_id.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>hello im a route /products/id</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ pages/products/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <LargeCardDisplay
+        v-for="{ id, title, snippet, cards } in largeCardInfo.slice(0, 1)"
+        :key="id"
+        :title="title"
+        :snippet="snippet"
+        :cards="cards"
+      />
+      <SmallCardDisplay
+        v-for="{ id, title, cards } in smallCardInfo"
+        :key="id"
+        :title="title"
+        :images="cards"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '../../data.js'
+
+export default {
+  data: () => ({
+    largeCardInfo: largeCardSections,
+    smallCardInfo: smallCardSections,
+  }),
+}
+</script>
+
+<style scoped></style>
